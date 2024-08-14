f390e9714be6fcc62cf0ccfda776842c2dcd5696
Adebowale Adam <adebowaleadam0@gmail.com>
2024-08-14 03:26:42 +0100

    useState with time intervals

--- src/components/01-useState/UseStateGotcha.jsx
+++ src/components/01-useState/UseStateGotcha.jsx
@@ -1,10 +1,54 @@
 import { useState } from "react";
+// using timeouts: such that it only takes a single state during thr timeout duration
+// const UseStateGotcha = () => {
+//   const [click, onClick] = useState(0);
+
+//   const increaseOnClick = () => {
+//     onClick(click + 1);
+
+//     console.log(click);
+//   };
+//   return (
+//     <>
+//       <h4>{click}</h4>
+//       <button type="button" className="btn" onClick={increaseOnClick}>
+//         increse
+//       </button>
+//     </>
+//   );
+// };
+
+// const UseStateGotcha = () => {
+//   const [click, onClick] = useState(0);
+
+//   const increaseOnClick = () => {
+//     setTimeout(() => {
+//       onClick(click + 1);
+//     }, 3000);
+
+//     console.log(click);
+//   };
+//   return (
+//     <>
+//       <h4>{click}</h4>
+//       <button type="button" className="btn" onClick={increaseOnClick}>
+//         increse
+//       </button>
+//     </>
+//   );
+// };
+
+// to fix this, the onclick function is set as a function which operates and update the value of click during the time interval.
 
 const UseStateGotcha = () => {
   const [click, onClick] = useState(0);
 
   const increaseOnClick = () => {
-    onClick(click + 1);
+    setTimeout(() => {
+      onClick((currentValue) => {
+        return currentValue + 1;
+      });
+    }, 3000);
 
     console.log(click);
   };

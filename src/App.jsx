b38d20447de7c23ca191dfd1d58c33a49a7a668b
Adebowale Adam <adebowaleadam0@gmail.com>
2024-09-03 22:46:49 +0100

    useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import UseStateGotcha from "./components/01-useState/UseStateGotcha";
 import UseStateObject from "./components/01-useState/UseStateObject";
+import UseEffectBasics from "./components/02-useEffect/useEffectBasics";
 
 function App() {
   return (
     <div className="container">
-      <UseStateGotcha />
+      <UseEffectBasics />
     </div>
   );
 }

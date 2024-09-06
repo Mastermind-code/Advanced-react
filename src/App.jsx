e199135b92f5fc9bd291d30461e6cf1da3d49fb2
Adebowale Adam <adebowaleadam0@gmail.com>
2024-09-06 23:10:18 +0100

    multiple return basics

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,12 @@ import UseStateGotcha from "./components/01-useState/UseStateGotcha";
 import UseStateObject from "./components/01-useState/UseStateObject";
 import FetchData from "./components/02-useEffect/fetchData";
 import UseEffectBasics from "./components/02-useEffect/useEffectBasics";
+import MultipleReturnsBasics from "./components/03-conditional-rendering/multipleReturnsBasics";
 
 function App() {
   return (
     <div className="container">
-      <FetchData />
+      <MultipleReturnsBasics />
     </div>
   );
 }

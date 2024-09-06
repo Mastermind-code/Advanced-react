f5a405b43b2e451e30a8d47ac04b044698d92874
Adebowale Adam <adebowaleadam0@gmail.com>
2024-09-06 23:58:20 +0100

    data destructuring

--- src/components/03-conditional-rendering/multipleReturnsFetchData.jsx
+++ src/components/03-conditional-rendering/multipleReturnsFetchData.jsx
@@ -4,12 +4,21 @@ const url = "https://api.github.com/users/QuincyLarson";
 const MultipleReturnsFetchData = () => {
   const [isLoading, setISLoading] = useState(true);
   const [isError, setIsError] = useState(false);
-  const [user, setUser] = useState(null);
+  const [{ name, avatar_url, bio }, setUser] = useState({
+    name: "",
+    avatar_url: "",
+    bio: "",
+  });
 
   useEffect(() => {
     const fetchData = async () => {
       try {
         const rss = await fetch(url);
+        if (!rss.ok) {
+          setIsError(true);
+          setISLoading(false);
+          return;
+        }
         const user = await rss.json();
         setUser(user);
       } catch (error) {
@@ -26,11 +35,12 @@ const MultipleReturnsFetchData = () => {
   if (isError) {
     return <h2>There was an error...</h2>;
   }
+  // const [name, bio, avatar_url] = user {will also work instead of changing it at the top.}
   return (
     <div>
-      <img src={user.avatar_url} alt={user.name} />
-      <h2>{user.name}</h2>
-      <p>{user.bio}</p>
+      <img src={avatar_url} alt={name} />
+      <h2>{name}</h2>
+      <p>{bio}</p>
     </div>
   );
 };

d37b1bb5d9feca7fe0a709b267dc0070328d088f
Adebowale Adam <adebowaleadam0@gmail.com>
2024-09-06 02:34:22 +0100

    fetchData

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import UseStateGotcha from "./components/01-useState/UseStateGotcha";
 import UseStateObject from "./components/01-useState/UseStateObject";
+import FetchData from "./components/02-useEffect/fetchData";
 import UseEffectBasics from "./components/02-useEffect/useEffectBasics";
 
 function App() {
   return (
     <div className="container">
-      <UseEffectBasics />
+      <FetchData />
     </div>
   );
 }

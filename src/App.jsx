ad2d3739edd4bd0130c0b60b334cb4e731cdc781
Adebowale Adam <adebowaleadam0@gmail.com>
2024-09-15 22:17:13 +0100

    correction

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,3 @@
-import UseStateGotcha from "./components/01-useState/UseStateGotcha";
-import UseStateObject from "./components/01-useState/UseStateObject";
-import FetchData from "./components/02-useEffect/fetchData";
-import UseEffectBasics from "./components/02-useEffect/useEffectBasics";
-import MultipleReturnsBasics from "./components/03-conditional-rendering/multipleReturnsBasics";
-import MultipleReturnsFetchData from "./components/03-conditional-rendering/multipleReturnsFetchData";
-import ShortCircuitExamples from "./components/03-conditional-rendering/shortCircuitExamples";
 import ShortCircuitOverview from "./components/03-conditional-rendering/shortCircuitOverview";
 
 function App() {

07f90c447d0e48047f7140596202ecec34fec86f
Adebowale Adam <adebowaleadam0@gmail.com>
2024-09-25 23:08:42 +0100

    components

--- src/components/05-leverage-javascript/Person.jsx
+++ src/components/05-leverage-javascript/Person.jsx
@@ -1,14 +1,14 @@
-// import React from "react";
+import React from "react";
 
-// export function Person({ name, nickName = "shakeAndBake", images }) {
-//   //   const img = images[0].small.url;
-//   //   const img = images && images[0] && images[0].small && images[0].small.url;
-//   const img = images?.[0]?.small?.url;
-//   return (
-//     <div>
-//       <img src={img} alt={name} style={{ width: "50px" }} />
-//       <h4>{name}</h4>
-//       <h4>{nickName}</h4>
-//     </div>
-//   );
-// }
+export function Person({ name, nickName = "shakeAndBake", images }) {
+  //   const img = images[0].small.url;
+  //   const img = images && images[0] && images[0].small && images[0].small.url;
+  const img = images?.[0]?.small?.url;
+  return (
+    <div>
+      <img src={img} alt={name} style={{ width: "50px" }} />
+      <h4>{name}</h4>
+      <h4>{nickName}</h4>
+    </div>
+  );
+}

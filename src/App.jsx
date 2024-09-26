aa7cd5679de3e3b7e69f994acb133b28dc186938
Adebowale Adam <adebowaleadam0@gmail.com>
2024-09-26 17:49:48 +0100

    multiple input

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import ControlledInputs from "./components/06-forms/ControlledInputs";
+import MultipleInputs from "./components/06-forms/MultipleInputs";
 import UserChallenge from "./components/06-forms/UserChallenge";
 
 function App() {
   return (
     <div className="container">
-      <UserChallenge />
+      <MultipleInputs />
     </div>
   );
 }

6d2cd5157df5b3fb6cd891a569a3d36c5106fcab
Adebowale Adam <adebowaleadam0@gmail.com>
2024-09-26 17:58:18 +0100

    checkbox

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import ControlledInputs from "./components/06-forms/ControlledInputs";
 import MultipleInputs from "./components/06-forms/MultipleInputs";
+import OtherInputs from "./components/06-forms/OtherInputs";
 import UserChallenge from "./components/06-forms/UserChallenge";
 
 function App() {
   return (
     <div className="container">
-      <MultipleInputs />
+      <OtherInputs />
     </div>
   );
 }

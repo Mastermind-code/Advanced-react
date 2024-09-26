93c6d943de32c3b748495a492808bf93e195a7bc
Adebowale Adam <adebowaleadam0@gmail.com>
2024-09-26 19:18:45 +0100

    formdata api

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import ControlledInputs from "./components/06-forms/ControlledInputs";
+import UncontrolledInputs from "./components/06-forms/FormData";
 import MultipleInputs from "./components/06-forms/MultipleInputs";
 import OtherInputs from "./components/06-forms/OtherInputs";
 import UserChallenge from "./components/06-forms/UserChallenge";
@@ -6,7 +7,7 @@ import UserChallenge from "./components/06-forms/UserChallenge";
 function App() {
   return (
     <div className="container">
-      <OtherInputs />
+      <UncontrolledInputs />
     </div>
   );
 }

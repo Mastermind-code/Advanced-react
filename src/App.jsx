de57ebdcf1bb19351fa27379380dcb5f700e2572
Adebowale Adam <adebowaleadam0@gmail.com>
2024-11-04 17:34:43 +0100

    payload

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,12 @@ import MultipleInputs from "./components/06-forms/MultipleInputs";
 import OtherInputs from "./components/06-forms/OtherInputs";
 import UserChallenge from "./components/06-forms/UserChallenge";
 import Navbar from "./components/09-context-api/Context api Custom hook/Navbar";
+import ReducerBasics from "./components/10-useReducer/NewReducer";
 
 function App() {
   return (
     <div className="container">
-      <Navbar />
+      <ReducerBasics />
     </div>
   );
 }

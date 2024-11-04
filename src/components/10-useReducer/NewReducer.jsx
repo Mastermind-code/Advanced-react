08aa28e2b200bd06d06998de8a9f5501a43fdfbf
Adebowale Adam <adebowaleadam0@gmail.com>
2024-11-04 17:34:43 +0100

    payload

---
+++ src/components/10-useReducer/NewReducer.jsx
@@ -0,0 +1,71 @@
+import React, { useReducer } from "react";
+import { data } from "../../data";
+
+const CLEAR_LIST = "CLEAR_LIST";
+const RESET_LIST = "RESET_LIST";
+const REMOVE_ITEM = "REMOVE_ITEM";
+
+const defaultState = {
+  people: data,
+};
+
+const reducer = (state, action) => {
+  if (action.type === CLEAR_LIST) {
+    return { ...state, people: [] };
+  }
+  if (action.type === RESET_LIST) {
+    return { ...state, people: data };
+  }
+  if (action.type === REMOVE_ITEM) {
+    let newPeople = state.people.filter(
+      (person) => person.id !== action.payload.id
+    );
+    return { ...state, people: newPeople };
+  }
+};
+const ReducerBasics = () => {
+  const [state, dispatch] = useReducer(reducer, defaultState);
+
+  const removeItem = (id) => {
+    dispatch({ type: REMOVE_ITEM, payload: { id } });
+  };
+  const resetList = () => {
+    dispatch({ type: RESET_LIST });
+  };
+
+  const clearList = () => {
+    dispatch({ type: CLEAR_LIST });
+  };
+  return (
+    <div>
+      {state.people.map((person) => {
+        const { id, name } = person;
+        return (
+          <div key={id} className="item">
+            <h4>{name}</h4>
+            <button onClick={() => removeItem(id)}>remove</button>
+          </div>
+        );
+      })}
+      {state.people.length < 1 ? (
+        <button
+          className="btn"
+          style={{ marginTop: "2rem" }}
+          onClick={resetList}
+        >
+          reset items
+        </button>
+      ) : (
+        <button
+          className="btn"
+          style={{ marginTop: "2rem" }}
+          onClick={clearList}
+        >
+          clear items
+        </button>
+      )}
+    </div>
+  );
+};
+
+export default ReducerBasics;
